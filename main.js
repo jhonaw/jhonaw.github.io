1c25c0cf15865868e3d028a3a0068c00e5cadd2e
jhonaw <jhonathansin@hotmail.com>
2019-05-08 18:55:49 -0300

    adicionando script.js

---
+++ main.js
@@ -0,0 +1,12 @@
+function left() {
+    var cor = document.querySelector("h1");
+    cor.setAttribute('style', 'text-align: left')
+}
+function center() {
+    var cor = document.querySelector("h1");
+    cor.setAttribute('style', 'text-align: center')
+}
+function right() {
+    var cor = document.querySelector("h1");
+    cor.setAttribute('style', 'text-align: right')
+}
