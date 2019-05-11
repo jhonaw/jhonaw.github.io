d304bf1200ad66f03316666172c5c51eabeb0b25
jhonaw <jhonathansin@hotmail.com>
2019-05-11 18:07:41 -0300

    feat: feita alteracao e reduçao no codigo

--- olho.js
+++ olho.js
@@ -1,46 +1,69 @@
-var imagens_src = ["./fecha.gif", "./abre.gif"];
+var imagens_src = ["./fecha.gif", "./abre.gif", "./aberto.gif", "./fechado.gif"];
 var olho = document.querySelector("#olho");
 var body = document.querySelector("body");
 var direcao = 0;
 
 
-async function trocaImagem() {
-    return new Promise( (resolve, reject) => {
-        if( direcao == 0 ) {
+//async function trocaImagem() {
+    //return new Promise( (resolve, reject) => {
+        //if( direcao == 0 ) {
+            //console.log(direcao);
+            //setTimeout( () => { resolve(imagens_src[0]) }, 400 );
+            //direcao = 1;
+       // } else if ( direcao == 1 ) {
+            //setTimeout( () => { resolve(imagens_src[1]) }, 400 );
+            //console.log(direcao);
+            //direcao = 2;
             
-            setTimeout( () => { resolve(imagens_src[0]) }, 400 );
-        } else if ( direcao == 1 ) {
-            setTimeout( () => { resolve(imagens_src[1]) }, 400 );
-        } else {
-            reject('Erro');
-        }
-    } )
+        //} else if(direcao == 2){
+	    //console.log(direcao);
+            //setTimeout( () => { resolve(imagens_src[2]) }, 400 );
+            //clearInterval(intervalo);
+	    //direcao = 0;
+        //}
+    //} )
         
+//}
+
+function fecha() {
+    if( direcao == 0 ) {
+        console.log(direcao);
+	olho.src = imagens_src[0];
+        setTimeout( () => { olho.src = imagens_src[3]; }, 300 );
+        direcao = 1;
+    }
+}
+
+function abre() {
+    if( direcao == 1 ) {
+        console.log(direcao);
+	olho.src = imagens_src[1];
+        setTimeout( () => { olho.src = imagens_src[2]; }, 300 );
+        direcao = 1;
+    }
 }
 
 
+var intervalo;
 
+//async function executa() {
+    //clearInterval(intervalo);
+    //intervalo = await setInterval(async () => {
+        //await trocaImagem()
+            //.then( response => {
+                //console.log(response);
+                //olho.src = response;
+            //} )
+    //}, 300 );
 
-async function executa() {
-    await setInterval(async () => {
-        await trocaImagem()
-            .then( response => {
-                console.log(response);
-                olho.src = response;
-                if(direcao == 0) {
-                    direcao = 1;
-                } else {
-                    direcao = 0;
-                }
-            } )
-    }, 300 );
-
-}
+//}
 
 
 
 body.onclick = () => {
-    executa();
+    //clearInterval(intervalo);
+    fecha();
+    abre();
 
 }
 
